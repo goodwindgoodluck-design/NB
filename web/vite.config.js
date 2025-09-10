3e1f00d9030d386e228abd6114b79ed35f2a2a0b
goodwindgoodluck-design <goodwindgoodluck@gmail.com>
2025-09-10 22:50:44 +1200

    chore: 配置Pages部署（vite.base=/NB, GitHub Actions）

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 项目发布在 https://goodwindgoodluck-design.github.io/NB/
+  // 因此需要设置基准路径为仓库名
+  base: '/NB/'
 })
